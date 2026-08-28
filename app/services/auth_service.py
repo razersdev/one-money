558966f2c606007400fdc19fa0a460db04f90556
razersdev <sopwankarawang@gmail.com>
2026-08-28 09:41:31 +0700

    menambahkan penyimpanan user ke database

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,7 +1,23 @@
 from app.models.user import UserRegister
+from app.database.connection import get_connection
 
 
 def register_user(user: UserRegister):
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO users (name, email, password)
+        VALUES (?, ?, ?)
+        """,
+        (user.name, user.email, user.password)
+)
+    
+    connection.commit()
+    connection.close()
+    
     return {
         "name": user.name,
         "email": user.email
